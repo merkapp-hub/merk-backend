93a2467935d933ad2368219c51efd30e4d234001
Vaibhav <vaibhav@example.com>
2025-07-01 13:01:55 +0530

    changes add

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -5,7 +5,7 @@ const authMiddleware = require('@middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post("/contact",authMiddleware("user"), contact.createContact);
+router.post("/contact",authMiddleware(["admin", "seller","user"]), contact.createContact);
 router.get("/contacts", authMiddleware(["admin", "seller"]), contact.getAllContacts);
 router.delete("/contact/:id",authMiddleware, contact.deleteContact);
 
